2338ec18dc80527f4db2f8718f819826367c66fd
barcoappofficial-hub <barcoappofficial@gmail.com>
2026-01-22 14:21:34 -0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+// ADICIONEI 'get' e 'child' AQUI NA IMPORTAÇÃO
+import { getDatabase, ref, push, set, onValue, remove, get, child } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDUtlcK_C22o0iNhHOt0JfdVF445Nbevy8",
+  authDomain: "catalago1-8ddfe.firebaseapp.com",
+  databaseURL: "https://catalago1-8ddfe-default-rtdb.firebaseio.com",
+  projectId: "catalago1-8ddfe",
+  messagingSenderId: "1019943701927",
+  appId: "1:1019943701927:web:e8d854c0c700b77d1ffa82"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+// ADICIONEI 'get' AQUI NA EXPORTAÇÃO
+export { db, ref, push, set, onValue, remove, get };
